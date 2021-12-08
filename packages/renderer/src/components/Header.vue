1892780ca2817e5a6cdc2dd61bed0849bc97bb9f
Coutaq <7tGu9ZQn>
2021-12-08 22:23:49 +0300

    asd

--- packages/renderer/src/components/Header.vue
+++ packages/renderer/src/components/Header.vue
@@ -5,12 +5,7 @@
     >
         <header-link :route="{ 'title': 'Категории', 'href': 'categories' }" :links="[]"></header-link>
         <header-link :route="{ 'title': 'Интересы', 'href': 'interests' }" :links="[]"></header-link>
-        <header-link
-            :route="{ 'title': 'Пользователи', 'href': 'user-list' }"
-            :links="[{
-                'title': 'Список', 'href': 'user-list'
-            }, { 'title': 'Добавить', 'href': 'user-new' }]"
-        ></header-link>
+        <header-link :route="{ 'title': 'Пользователи', 'href': 'users' }"></header-link>
     </div>
 </template>
 <script lang= "ts">
